43b047daafcb43e3886f630545b2225a279e408a
Maxi <maaxii.cb@hotmail.com>
2018-11-04 11:54:03 -0300

    rutas

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,8 @@
 Rails.application.routes.draw do
 
-<<<<<<< HEAD
-=======
-  get 'auction/create'
-  get 'auction/new'
-  	get 'residences/index'
->>>>>>> master
-    root 'main#index' #redirecciona a la pagina principal
+  	get 'auction/create'
+  	get 'auction/new'
+	root 'main#index' #redirecciona a la pagina principal
   	get 'residences/index'
     get '/faq', to:'preguntasfrecuentes#faq' #ruta para poder acceder a las faq
 
