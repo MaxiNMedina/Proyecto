b3926b0e31cbbea73a70cd84e22980f4ddac43ce
Maxi <maaxii.cb@hotmail.com>
2018-11-04 11:37:00 -0300

    nada

--- app/models/residence.rb
+++ app/models/residence.rb
@@ -1,9 +1,4 @@
-<<<<<<< HEAD
-class Residence < ApplicationRecord
-    default_scope -> { order :name }
-=======
 class Residence < ApplicationRecord	
 	default_scope -> { order :nombre }
 	has_many :auctions
->>>>>>> master
 end
