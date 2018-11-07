d48e47a02b7d4ed2718e00f9cb0a2705bcd70598
Corsino Alexandre <corsinu.alexandre@gmail.com>
2018-11-07 15:44:36 -0300

    boton volver en faq

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ApplicationRecord
-  #many to many relation with users
+  #many to many relation with auctions
   has_many :created_auctions, class_name: "Auction", foreign_key: :user_id
   has_and_belongs_to_many :auctions
 end
