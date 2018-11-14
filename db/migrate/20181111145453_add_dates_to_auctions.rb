8d8221f465a22a3e6a2ddb557ded4337aaa850aa
Maxi <maaxii.cb@hotmail.com>
2018-11-14 17:35:48 -0300

    Update

--- db/migrate/20181111145453_add_dates_to_auctions.rb
+++ db/migrate/20181111145453_add_dates_to_auctions.rb
@@ -1,6 +1,6 @@
 class AddDatesToAuctions < ActiveRecord::Migration[5.2]
   def change
-  	add_column :auctions, :dateStart, :date
-  	add_column :auctions, :dateEnd, :date
+  	#add_column :auctions, :dateStart, :date
+  	#add_column :auctions, :dateEnd, :date
   end
 end
