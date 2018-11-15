ad59c869d3b2a2594bf4a09daad73c89c93ad412
Maxi <maaxii.cb@hotmail.com>
2018-11-15 20:44:06 -0300

    residencias arregladas, sesiones funcionando

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
 
 
-  devise_for :users
+  	devise_for :users
 	root 'main#index' #redirecciona a la pagina principal
 
     get '/faq', to:'preguntasfrecuentes#faq' #ruta para poder acceder a las faq
