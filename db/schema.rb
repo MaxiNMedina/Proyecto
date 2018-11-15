28f756d590fa197be55faaddbf583cc28d7836b5
Maxi <maaxii.cb@hotmail.com>
2018-11-15 14:30:34 -0300

    Update

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_11_11_145453) do
+ActiveRecord::Schema.define(version: 2018_11_15_172326) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -51,6 +51,7 @@ ActiveRecord::Schema.define(version: 2018_11_11_145453) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.boolean "isAdmin"
+    t.string "name"
   end
 
   add_foreign_key "auctions", "residences"
