6c153340622c5fb3ecf5a34db7ec6d50064df0c3
Maxi <maaxii.cb@hotmail.com>
2018-11-20 08:38:37 -0300

    todo ok

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 2018_11_17_215134) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "auctions", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
@@ -61,4 +64,7 @@ ActiveRecord::Schema.define(version: 2018_11_17_215134) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "auctions", "residences"
+  add_foreign_key "auctions_users", "auctions"
+  add_foreign_key "auctions_users", "users"
 end
