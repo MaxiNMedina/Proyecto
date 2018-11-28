701e3c2d92e338d8cbc2b91f4319ad66e4ae144e
Corsino Alexandre <corsinu.alexandre@gmail.com>
2018-11-27 22:30:18 -0300

    devise error en header, devise ressource en application controller, cambio de imagen

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,4 +6,31 @@ class ApplicationController < ActionController::Base
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:userName, :password, :credits, :isAdmin, :isPremium ])
   end
+
+	######## PARA PONER ERRORES DE DEVISE EN header
+	### https://pupeno.com/2016/04/26/show-a-devise-log-in-or-sign-up-forms-in-another-page/
+
+	def new
+  end
+
+  def resource_name
+    :user
+  end
+  helper_method :resource_name
+
+  def resource
+    @resource ||= User.new
+  end
+  helper_method :resource
+
+  def devise_mapping
+    @devise_mapping ||= Devise.mappings[:user]
+  end
+  helper_method :devise_mapping
+
+  def resource_class
+    User
+  end
+  helper_method :resource_class
+
 end
